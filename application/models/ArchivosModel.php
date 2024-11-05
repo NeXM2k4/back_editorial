b6df009f7149fd75409e662f1f11d3d23fbfddd3
NeX <155313145+NeXM2k4@users.noreply.github.com>
2024-11-05 11:04:26 -0600

    Add files via upload

---
+++ application/models/ArchivosModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+class File{
+
+    public function get_files_by_volume($volume_id){
+
+
+
+    }
+
+    public function get_file($id){
+
+
+
+    }
+
+    public function save_file($data){
+
+
+    }
+
+    public function delete_file($id){
+
+
+        
+    }
+
+}
---
+++ application/models/PublicacionesModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+class Volume{
+
+    public function get_volumes_by_collection($collection_id){
+
+
+
+    }
+
+    public function get_volume($id){
+
+
+
+    }
+
+    public function create_volume($data){
+
+
+
+    }
+
+    public function update_volume($id, $data){
+
+
+
+    }
+
+    public function delete_volume($id){
+
+
+        
+    }
+
+}
